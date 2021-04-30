7e618be7d8e5456c4edd35ec49abcf5adb7e574a
smatipanya <82169595+smatipanya@users.noreply.github.com>
2021-04-30 20:23:53 +0700

    commit

--- pgslot/js/notification_templates.js
+++ pgslot/js/notification_templates.js
@@ -1,4 +1,4 @@
-const scenarios = {
+var scenarios = {
     register_bank_recommendation: "register_bank_recommendation",
     wallet_slip_warning: "wallet_slip_warning",
     autoplay_maintenance: "autoplay_maintenance",
@@ -14,59 +14,46 @@ function notify(scenario){
     switch(scenario){
         case scenarios.register_bank_recommendation:
             window.createNotification({
-                theme: 'information',
-                showDuration: 100000,
-                closeOnClick: false,
-                onclick: function(event){ location.href = 'https://lin.ee/lrYsHQD'; }
+                theme: 'info',
+                showDuration: false,
+                closeOnClick: true,
+                displayCloseButton: true,
+                positionClass: 'nfc-top-right'
             })({
                 title: 'คำแนะนำในการเลือกธนาคารฝาก',
                 message: ' '
             });
             window.createNotification({
                 theme: 'success',
-                showDuration: 100000,
-                closeOnClick: false,
-                onclick: function(event){ location.href = 'https://lin.ee/lrYsHQD'; }
+                showDuration: false,
+                closeOnClick: true,
+                displayCloseButton: true,
+                positionClass: 'nfc-top-right'
             })({
-                title: '',
-                message: 'ฝากทศนิยม:  เครดิตเข้าออโต้  ไม่ต้องส่งสลิป  โอนได้จากทุกธนาคาร'
-            });
-            window.createNotification({
-                theme: 'success',
-                showDuration: 100000,
-                closeOnClick: false,
-                onclick: function(event){ location.href = 'https://lin.ee/lrYsHQD'; }
-            })({
-                title: '',
-                message: 'ฝากออโต้ปกติ:  เครดิตเข้าออโต้  ไม่ต้องส่งสลิป  โอนได้จากทุกธนาคารยกเว้นไทยพาณิชย์'
+                title: 'ฝากทศนิยม (จากทุกธนาคาร) หรือ ฝากแบบปกติ (จากทุกธนาคาร ยกเว้นไทยพาณิชย์)',
+                message: 'เครดิตเข้าออโต้  ภายใน 1 นาที  ไม่ต้องส่งสลิปค่าา'
             });
             window.createNotification({
                 theme: 'warning',
-                showDuration: 100000,
-                closeOnClick: false,
-                onclick: function(event){ location.href = 'https://lin.ee/lrYsHQD'; }
+                showDuration: false,
+                closeOnClick: true,
+                displayCloseButton: true,
+                positionClass: 'nfc-top-right'
             })({
-                title: '',
-                message: 'ฝากออโต้ปกติ:  หากโอนผ่านไทยพาณิชย์  เครดิตไม่เข้าออโต้  แจ้งสลิปที่แอดมินทุกยอดฝาก'
-            });
-            window.createNotification({
-                theme: 'warning',
-                showDuration: 100000,
-                closeOnClick: false,
-                onclick: function(event){ location.href = 'https://lin.ee/lrYsHQD'; }
-            })({
-                title: '',
-                message: 'ฝากผ่านทรูวอลเล็ต:  เครดิตไม่เข้าออโต้  แจ้งสลิปที่แอดมินทุกยอดฝาก'
+                title: 'ฝากแบบปกติผ่านไทยพาณิชย์ หรือ ฝากผ่านทรูวอลเล็ต',
+                message: 'เครดิตไม่เข้าออโต้  แจ้งสลิปที่แอดมินทุกยอดฝากนะคะ'
             });
             return;
         case scenarios.wallet_slip_warning:
             window.createNotification({
-                theme: 'information',
-                showDuration: 100000,
+                theme: 'info',
+                showDuration: false,
                 closeOnClick: false,
-                onclick: function(event){ location.href = 'https://lin.ee/lrYsHQD'; }
+                displayCloseButton: false,
+                onclick: function(event){ location.href = 'https://lin.ee/lrYsHQD'; },
+                positionClass: 'nfc-top-right'
             })({
-                title: 'หากลูกค้าโอนผ่านทรูวอลเล็ต หรือ โอนด้วยธ.ไทยพาณิชย์แต่ไม่เลือกแบบทศนิยม',
+                title: 'หากลูกค้าฝากแบบปกติผ่านไทยพาณิชย์ หรือ ฝากผ่านทรูวอลเล็ต',
                 message: 'คลิกที่นี่ เพื่อแจ้งสลิปที่แอดมินได้เลยจ้า'
             });
             return;
@@ -85,18 +72,20 @@ function notify(scenario){
         case scenarios.scb_unstable:
             window.createNotification({
                 theme: 'warning',
-                showDuration: 100000,
-                closeOnClick: false,
-                onclick: function(event){ location.href = 'https://lin.ee/lrYsHQD'; }
+                showDuration: false,
+                closeOnClick: true,
+                displayCloseButton: true,
+                positionClass: 'nfc-top-right'
             })({
                 title: 'ขณะนี้ ระบบธนาคารไทยพาณิชย์ขัดข้อง อาจส่งผลให้การฝากบางรายการล่าช้ากว่าปกติ (เฉพาะลูกค้าที่ฝากด้วยธ.ไทยพาณิชย์)',
                 message: 'หากเกิน 10 นาที เครดิตยังไม่เข้า สามารถส่งสลิปให้แอดมินตรวจสอบได้เลยค่ะ'
             });
             window.createNotification({
-                theme: 'information',
-                showDuration: 100000,
-                closeOnClick: false,
-                onclick: function(event){ location.href = 'https://lin.ee/lrYsHQD'; }
+                theme: 'info',
+                showDuration: false,
+                closeOnClick: true,
+                displayCloseButton: true,
+                positionClass: 'nfc-top-right'
             })({
                 message: 'ฝากครั้งต่อไป แอดแนะนำให้เลือกฝากแบบทศนิยมนะคะ (เครดิตเข้าออโต้ภายใน 1 นาที ไม่ต้องส่งสลิป)'
             });
